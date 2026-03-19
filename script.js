e4dd4645e7ea5c32e54ee05b8906227e5f6db1d5
Ajit Saxena <113728833+ajit638823@users.noreply.github.com>
2026-03-20 01:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-var student = {
-  name: "John",
-  age: 20,
-  city: "Delhi"
+const student = {
+  name: "John"
 };
 
 function getKeys(obj) {
   return Object.keys(obj);
 }
-console.log(getKeys(student));
